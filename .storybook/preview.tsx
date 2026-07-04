f1f6986b2bd7453e8c7142ec3a0e893bcb4c3310
Michael Jonathan <mjonathannn98@gmail.com>
2026-07-04 19:02:13 -0300

    fix: force a white background in Storybook previews

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,12 +1,22 @@
 import type { Preview } from "@storybook/react-vite"
+import { createGlobalStyle } from "styled-components"
+
+import { colors } from "@/foundation"
 
 import GlobalStyle from "../src/styles/GlobalStyle"
+
+const StorybookBackground = createGlobalStyle`
+  body {
+    background: ${colors.neutral[0]};
+  }
+`
 
 const preview: Preview = {
   decorators: [
     (Story) => (
       <>
         <GlobalStyle />
+        <StorybookBackground />
         <Story />
       </>
     ),
